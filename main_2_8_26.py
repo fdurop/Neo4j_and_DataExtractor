41c1e8f3c57e309676403ca8c328018e80647543
lin732 <3348397937@qq.com>
2025-09-04 20:43:01 +0800

    Add files via upload
    修改：
    实现最后的实体-关系-属性提取和neo4j数据库对接的流程，
    
    优化：
    更换更好用的大模型api

---
+++ main_2_8_26.py
@@ -0,0 +1,59 @@
+import Get_neo4j_2
+import Data_Extractor_6
+import sys
+import io
+import os
+import json
+import pandas as pd
+
+
+
+if __name__ == '__main__':
+
+    # 初始化抽取器 (启用CLIP需要先安装transformers和torch)
+    os.environ["ZHIPUAI_API_KEY"] = "2cee1433c4984becab7c30701b2c5fc1.bwFtIqU9qHpWKZDb"
+
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+    extractor = Data_Extractor_6.EnhancedMultimodalExtractor(
+        use_clip=True,
+        domain_config_path=r"E:\Neo4j\neo4j-community-5.26.0-windows\neo4j-community-5.26.0-windows\neo4j-community-5.26.0\import\domain_config.json",
+        use_zhipuai=True  # 启用智谱AI API
+    )
+
+    file_path = r"E:\Neo4j\neo4j-community-5.26.0-windows\neo4j-community-5.26.0-windows\neo4j-community-5.26.0\import\week4_p65.json"
+    # 执行抽取
+    result = extractor.process_multimodal_data([file_path])
+
+    entities = result.entities
+    relations = result.relationships
+    attributes = result.attributes
+
+
+    # 转换数据
+    nodes_df, rels_df = Data_Extractor_6.convert_extracted_to_dataframes(entities, relations)
+
+    print("节点数据:")
+    print(nodes_df.head())
+    print("\n关系数据:")
+    print(rels_df.head())
+
+    try:
+        kg = Get_neo4j_2.Neo4jTeamCollaborator(
+            uri="bolt://localhost:7687",
+            user="neo4j",
+            password="Wangshuxvan@1"
+        )
+
+        # 从DataFrame直接导入数据
+        kg.import_from_dataframe(nodes_df=nodes_df, rels_df=rels_df)
+
+    except Exception as e:
+        print(f"执行过程中出错: {e}")
+        import traceback
+
+        traceback.print_exc()
+    finally:
+        if 'kg' in locals():
+            kg.close()
+            print("数据库连接已关闭")
