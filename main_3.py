c826bd1a5bcc827a2dffffd9a404dfe503d9607d
lin732 <3348397937@qq.com>
2025-09-16 19:46:31 +0800

    Update main_3.py

--- main_3.py
+++ main_3.py
@@ -25,6 +25,11 @@ if __name__ == '__main__':
         deepseek_model="deepseek-chat",  # 使用DeepSeek的chat模型
         api_key=api_key  # 传入API Key
     )
+
+    # 上面这些是完成数据抽取代码的初始化（包括调用deepseek大模型）
+
+    
+    # 下面这个注释是数据抽取的过程，暂时可以不管
     '''
     file_path = r"E:/Neo4j/neo4j-community-5.26.0-windows/neo4j-community-5.26.0-windows/neo4j-community-5.26.0\import\week4_p65.json"
     # 执行抽取
@@ -34,29 +39,22 @@ if __name__ == '__main__':
     relations = result.relationships
     attributes = result.attributes
 
-
-
     # 转换数据
     nodes_df, rels_df = Data_Extractor_7.convert_extracted_to_dataframes(entities, relations)
-    
-    print("节点数据:")
-    print(nodes_df.head())
-    print("\n关系数据:")
-    print(rels_df.head())
     '''
-
     try:
         kg = Get_neo4j_2.Neo4jTeamCollaborator(
             uri="bolt://101.132.130.25:7687",
             user="neo4j",
             password="wangshuxvan@1"
         )
-
+        # 图数据库连接初始化
+        
         # 从DataFrame直接导入数据
         # kg.import_from_dataframe(nodes_df=nodes_df, rels_df=rels_df)
-        answer = extractor.extract_keywords_with_deepseek("爱因斯坦发现了相对论")
-        prompt = kg.semantic_search(answer)
-        print(prompt)
+        answer = extractor.extract_keywords_with_deepseek("爱因斯坦发现了相对论") # 使用数据抽取那个类里面的deepseek做关键词抽取工作，返回关键词列表
+        prompt = kg.semantic_search(answer)                                     # 对于关键词列表查找，返回“主谓宾句子”的列表
+        print(prompt)          # 展示结果，可有可无
 
     except Exception as e:
         print(f"执行过程中出错: {e}")
@@ -67,3 +65,4 @@ if __name__ == '__main__':
         if 'kg' in locals():
             kg.close()
             print("数据库连接已关闭")
+
